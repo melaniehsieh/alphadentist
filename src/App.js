e2c36c15cc0d19e91c11b0bf87e4e5ebd470d5a7
melaniehsieh <mpwh2004@gmail.com>
2021-01-24 07:32:06 +0800

    backend test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Landing from "./components/Landing/Landing";
 import AddForm from "./components/Form/AddForm";
 
 function App() {
+  const [prediction, setPrediction] = useState(null);
+
+  useEffect(() => {
+    fetch("/predict").then((res) => {
+      res.json().then((data) => {
+        setPrediction(data);
+      });
+    });
+  }, []);
+
   return (
     <div>
+      <h1>{prediction}</h1>
       <Landing />
       <AddForm />
     </div>
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -5,8 +5,14 @@ import { Link, animateScroll as scroll } from "react-scroll";
 const Landing = () => {
   return (
     <div id="section1">
-      <h1>Name</h1>
-      <h3>description</h3>
+      <h1 style={{ fontSize: "50px" }}>AlphaDentist</h1>
+      <h3>
+        Welcome to AlphaDentist! An online platform for users to determine their
+        likelihood of possessing oral illnesses. We understand your concerns and
+        hope to make dental diagnoses free and accessible to all members of the
+        community. Simply upload your patient information, a clear image of your
+        teeth and gums, and we will take care of the rest!
+      </h3>
       <Link
         activeClass="active"
         to="section2"
